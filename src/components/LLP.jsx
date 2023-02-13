347d191f16041bb4b27fb6dff4cf3e67608e9986
Alan Baltabay <baltabay123@gmail.com>
2023-02-13 22:21:21 +0600

    feat: add and style TIN input

--- src/components/LLP.jsx
+++ src/components/LLP.jsx
@@ -4,6 +4,7 @@ export default function LLP(props) {
 	const [isDropdown, setIsDropdown] = useState(false);
 	const [formData, setFormData] = useState({
 		taxation: props.taxations.at(0).full,
+		tinInput: '',
 	});
 
 	const dropdown = (
@@ -26,12 +27,20 @@ export default function LLP(props) {
 		setIsDropdown(false);
 	}
 
+	function handleTinInputChange(event) {
+		setFormData((prev) => ({
+			...prev,
+			[event.target.name]: event.target.value,
+		}));
+	}
+
 	return (
 		<>
 			<div>
 				<label className='label-taxation' htmlFor='taxation'>
 					Выберите систему налогообложения
 				</label>
+
 				<div className='select-container'>
 					<div
 						className='taxation'
@@ -42,6 +51,22 @@ export default function LLP(props) {
 					</div>
 
 					{isDropdown && dropdown}
+				</div>
+			</div>
+
+			<div className='tin-input-label-container'>
+				<label className='label-taxation' htmlFor='tin-input'>
+					Введите ИИН/БИН
+				</label>
+
+				<div className='select-container'>
+					<input
+						className='tin-input'
+						id='tin-input'
+						value={formData.tinInput}
+						name='tinInput'
+						onChange={handleTinInputChange}
+					/>
 				</div>
 			</div>
 		</>
